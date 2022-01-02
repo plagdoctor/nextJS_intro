cc6b5a9a04d8b2f3b074c86652590b4985e12a3d
jinyoung <danroom85@gmail.com>
2022-01-02 20:12:06 +0900

    finish project

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,7 @@ import Seo from "../components/Seo";
 export default function Home({ results }) {
     const router = useRouter();
     const onClick = (id, title) => {
-        router.push(
-            {pathname: `/movies/${id}`,
-            query: {
-                title,
-            }
-        },
-            `/movies/${id}`
-        );
+        router.push(`/movies/${title}/${id}`);
     }
     return (
         <div className="container">
@@ -22,14 +15,7 @@ export default function Home({ results }) {
             <div onClick={()=>onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
             <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
             <h4>
-                <Link href={
-                    {pathname: `/movies/${movie.id}`,
-                    query: {
-                        title: movie.original_title,
-                    }
-                }}
-                as={`/movies/${movie.id}`}
-                >
+                <Link href={`/movies/${movie.original_title}/${movie.id}`} >
                 <a>{movie.original_title}</a>
                 </Link> 
             </h4>
--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -1,8 +1,23 @@
 import { useRouter } from "next/router";
+import Seo from "../../components/Seo";
 
-export default function Detail() {
+export default function Detail({params}) {
+    console.log("Detail first!");
     const router = useRouter();
-    console.log(router);
 
-    return (<h4>{router.query.title || "Loading..."}</h4>);
+    console.log("Detail Second!");
+    const [title,id] = params || [];
+    return (
+        <div>
+            <Seo title ={title} />
+            <h4>{title}</h4>
+        </div>
+    );
+}
+
+export function getServerSideProps({params: {params} }) {
+    console.log("getServerSideProrps first!");
+    return {
+        props: {params},
+    };
 }
