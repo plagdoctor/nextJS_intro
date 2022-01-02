ce6d7ad9e2f83d759981678a551668dd9aad2519
jinyoung <danroom85@gmail.com>
2022-01-02 11:13:54 +0900

    catching url

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,63 @@
-import { useEffect, useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
-  return (
-    <div className="container">
-      <Seo title="Home" />
-      {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+    const router = useRouter();
+    const onClick = (id, title) => {
+        router.push(
+            {pathname: `/movies/${id}`,
+            query: {
+                title,
+            }
+        },
+            `/movies/${id}`
+        );
+    }
+    return (
+        <div className="container">
+        <Seo title="Home" />
+        {results?.map((movie) => (
+        
+            <div onClick={()=>onClick(movie.id, movie.original_title)} className="movie" key={movie.id}>
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>
+                <Link href={
+                    {pathname: `/movies/${movie.id}`,
+                    query: {
+                        title: movie.original_title,
+                    }
+                }}
+                as={`/movies/${movie.id}`}
+                >
+                <a>{movie.original_title}</a>
+                </Link> 
+            </h4>
+            </div>
+            
+        ))}
+        <style jsx>{`
+            .container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            padding: 20px;
+            gap: 20px;
+            }
+            .movie img {
+            max-width: 100%;
+            border-radius: 12px;
+            transition: transform 0.2s ease-in-out;
+            box-shadow: rgba(0, 0, 0, 0.5) 0px 4px 12px;
+            }
+            .movie:hover img {
+            transform: scale(1.05) translateY(-10px);
+            }
+            .movie h4 {
+            font-size: 18px;
+            text-align: center;
+            }
+        `}</style>
         </div>
-      ))}
-      <style jsx>{`
-        .container {
-          display: grid;
-          grid-template-columns: 1fr 1fr;
-          padding: 20px;
-          gap: 20px;
-        }
-        .movie img {
-          max-width: 100%;
-          border-radius: 12px;
-          transition: transform 0.2s ease-in-out;
-          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
-        }
-        .movie:hover img {
-          transform: scale(1.05) translateY(-10px);
-        }
-        .movie h4 {
-          font-size: 18px;
-          text-align: center;
-        }
-      `}</style>
-    </div>
   );
 }
 
